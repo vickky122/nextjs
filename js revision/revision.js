3aac2d34e3eb3b4cecedc485cfcbb5444fa1f939
Vikrant Yadav <vikrantmani45@gmail.com>
2024-12-22 10:18:45 +0530

    primitive/ value type

--- js revision/revision.js
+++ js revision/revision.js
@@ -1,2 +1,26 @@
-console.log("hello");
-console.log(2+3);
+// console.log("hello");
+// console.log(2+3);
+
+
+//Primitive/value types
+//variable
+let x="vikrant";
+console.log(x);
+console.log(typeof x);
+x=null;
+console.log(typeof x);//changes into object
+//we can change the type of the variables thats why it is known as dynamic variable
+
+let y =1;
+console.log(y);
+console.log(typeof y);
+
+let z=true;
+console.log(z);
+console.log(typeof z);
+
+
+let w;
+console.log(w);
+w="vikrant";
+console.log(typeof w);
